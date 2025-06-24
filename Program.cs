87a3d9788e5fce17d1d40427132fc7c97ab742ec
AtaraN <0583280005a@gmail.com>
2025-06-24 10:53:04 +0300

    deploy

--- Program.cs
+++ Program.cs
@@ -121,11 +121,11 @@ using (var scope = app.Services.CreateScope())
         {
             context.Categories.AddRange(new[]
             {
-            new Category { Name = "פירות וירקות" },
-            new Category { Name = "מוצרי חלב" },
-            new Category { Name = "מאפים" },
-            new Category { Name = "דגים ובשר" },
-            new Category { Name = "חטיפים" }
+            new Category { Name = "Vegetables" },
+            new Category { Name = "Dairy Products" },
+            new Category { Name = "Baked Goods" },
+            new Category { Name = "Snacks" },
+            new Category { Name = "Meat and Fish" }
         });
 
             context.SaveChanges();
